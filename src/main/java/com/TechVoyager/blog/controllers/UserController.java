260776d6fd2479f0533ba5ea8a451ab5ebc7c05d
zareef <zareefjafar31@gmail.com>
2023-04-22 19:34:33 -0600

    Add validation

--- src/main/java/com/TechVoyager/blog/controllers/UserController.java
+++ src/main/java/com/TechVoyager/blog/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.TechVoyager.blog.controllers;
 import com.TechVoyager.blog.payloads.ApiResponse;
 import com.TechVoyager.blog.payloads.UserDto;
 import com.TechVoyager.blog.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +20,14 @@ public class UserController {
     private UserService userService;
     //Post-create user
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
 
     //PUT-update user
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid)
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid)
     {
         UserDto updatedUser = this.userService.updateUser(userDto, uid);
         return ResponseEntity.ok(updatedUser);
--- src/main/java/com/TechVoyager/blog/payloads/UserDto.java
+++ src/main/java/com/TechVoyager/blog/payloads/UserDto.java
@@ -1,5 +1,9 @@
 package com.TechVoyager.blog.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +13,19 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private int id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Username must be minimum 2 characters")
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "Please enter valid email id")
+    @Email(message =  "Please enter valid email id")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,12}$",
+            message = "password must be min 4 and max 12 length containing at least 1 uppercase, 1 lowercase, 1 special character and 1 digit ")
     private String password;
+
+    @NotEmpty(message = "Please add a description")
     private String about;
 }
